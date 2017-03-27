16836b339f9a3c0fb61ecee612ca59a2b113268c
Ushio <jylzs369@163.com>
2017-03-27 10:39:06 +0800

    add files

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssnano = require('cssnano'),
+    rename = require('gulp-rename'),
+    stylelint = require('stylelint'),
+    reporter = require('postcss-reporter');
+
+gulp.task('styles', function () {
+    return gulp.src('src/*.css')
+        .pipe(postcss([autoprefixer]))
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('rename', ['styles'], function () {
+    return gulp.src('dist/index.css')
+        .pipe(postcss([cssnano]))
+        .pipe(rename('index.min.css'))
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('lint-styles', function () {
+    return gulp.src('src/*.css')
+        .pipe(postcss([stylelint({
+            'rules': {
+                'color-no-invalid-hex': true
+            }
+        }),
+        reporter({
+            clearMessages: true
+        })
+    ]))
+});
+
+gulp.task('default', ['styles', 'rename', 'lint-styles']);
+
+var watcher = gulp.watch('src/*.css', ['default']);
+watcher.on('change', function (event) {
+    console.log('File' + event.path + 'was' + event.type + ',running tasks...');
+});
