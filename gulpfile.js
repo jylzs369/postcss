5bc06d41f56a38fcba8b8a7b94d8702e1894c239
Ushio <jylzs369@163.com>
2017-03-28 11:50:29 +0800

    add icons & nesting

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,16 @@ var gulp = require('gulp'),
     cssnano = require('cssnano'),
     rename = require('gulp-rename'),
     stylelint = require('stylelint'),
-    reporter = require('postcss-reporter')
-    cssvariables = require('postcss-css-variables');
+    reporter = require('postcss-reporter'),
+    cssvariables = require('postcss-css-variables')
+    cssmixins = require('postcss-mixins'),
+    calc = require('postcss-calc')
+    eachloop = require('postcss-each'),
+    nesting = require('postcss-nesting');
 
 gulp.task('styles', function () {
     return gulp.src('src/*.css')
-        .pipe(postcss([autoprefixer, cssnano, cssvariables()]))
+        .pipe(postcss([autoprefixer, nesting, cssvariables, cssmixins, calc, eachloop]))
         .pipe(sourcemaps.init())
         .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('dist'));
@@ -19,6 +23,7 @@ gulp.task('styles', function () {
 gulp.task('rename', ['styles'], function () {
     return gulp.src('dist/index.css')
         .pipe(rename('index.min.css'))
+        .pipe(postcss([cssnano]))
         .pipe(sourcemaps.init())
         .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('dist'));
@@ -37,9 +42,9 @@ gulp.task('lint-styles', function () {
     ]))
 });
 
-gulp.task('default', ['styles', 'rename', 'lint-styles']);
+gulp.task('default', ['lint-styles', 'styles', 'rename']);
 
 var watcher = gulp.watch('src/*.css', ['default']);
 watcher.on('change', function (event) {
-    console.log('File' + event.path + 'was' + event.type + ',running tasks...');
+    console.log('File' + event.path + ' was ' + event.type + ',running tasks...');
 });
